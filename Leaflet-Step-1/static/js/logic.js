1c7acb9936750d0f348a168888d9127b5b0d0104
unknown <cayelsie@gmail.com>
2021-04-16 21:21:53 -0400

    some work on colors

--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -23,6 +23,36 @@ L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
 //Link to get geojson data
 var queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
 
+
+
+// Function that will determine the color of the marker based on depth of earth
+function chooseColor(depth) {
+
+  if (depth < 10 & depth >= -10) {
+      return "green";
+  } 
+  else if (30 < depth & depth <= 10) {
+      return "pink";
+  } 
+  
+  else if (50 < depth & depth <= 30) {
+      return "#12E510";
+  } 
+  
+  else if (70 < depth & depth <= 50) {
+      return "#DCEB0C";
+  } 
+  
+  else if (90 < depth & depth <= 70) {
+      return "#ff7800"
+  } 
+  
+  else {
+      return "#F02908";
+  };
+}
+
+
 // // Perform a GET request to the query URL
 d3.json(queryUrl).then(function (data) {
 
@@ -35,7 +65,7 @@ d3.json(queryUrl).then(function (data) {
 
         var geojsonMarkerOptions = {
         radius: (feature.properties.mag) * 5,
-        fillColor: "#ff7800",
+        fillColor: chooseColor(feature.geometry.coordinates[2]),
         color: "#000",
         weight: 1,
         opacity: 1,
@@ -49,8 +79,40 @@ d3.json(queryUrl).then(function (data) {
     // Binding a pop-up to each layer with place and time the earthquake occurred
     onEachFeature: function (feature, layer) {
 
+      console.log(feature.geometry.coordinates[2]);
+      
+      // depths.forEach(function (depth) {
+      //   console.log(Math.max(depth));
+
+      // });
+
       layer.bindPopup("Site of earthquake: " + feature.properties.place + "<br>Time occurred: " +
         + new Date(feature.properties.time) +"<br>Magnitude: " + feature.properties.mag);
     }
   }).addTo(myMap);
+
+    // Set up the legend
+    var legend = L.control({ position: "bottomright" });
+    legend.onAdd = function() {
+      var div = L.DomUtil.create("div", "info legend");
+      var limits = geojson.options.limits;
+      var colors = geojson.options.colors;
+      var labels = [];
+  
+      // Add min & max
+      var legendInfo = "<h1>Median Income</h1>" +
+        "<div class=\"labels\">" +
+          "<div class=\"min\">" + limits[0] + "</div>" +
+          "<div class=\"max\">" + limits[limits.length - 1] + "</div>" +
+        "</div>";
+  
+      div.innerHTML = legendInfo;
+  
+      limits.forEach(function(limit, index) {
+        labels.push("<li style=\"background-color: " + colors[index] + "\"></li>");
+      });
+  
+      div.innerHTML += "<ul>" + labels.join("") + "</ul>";
+      return div;
+    };
   });
